c36cfc1f0e8d3bd92665cf108646526418e18e5b
johnvesslyalti <altijohnvessly@gmail.com>
2026-02-22 11:36:36 +0530

    feat: Integrate Clerk authentication and Convex for user management, including new user mutations and a client provider.

---
+++ convex/users.ts
@@ -0,0 +1,31 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const createUser = mutation({
+  args: {
+    clerkId: v.string(),
+    name: v.string(),
+    imageUrl: v.string(),
+  },
+
+  handler: async (ctx, args) => {
+    const existingUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
+      .first();
+
+    if (existingUser) return existingUser._id;
+
+    return await ctx.db.insert("users", {
+      clerkId: args.clerkId,
+      name: args.name,
+      imageUrl: args.imageUrl,
+    });
+  },
+});
+
+export const getUsers = query({
+  handler: async (ctx) => {
+    return await ctx.db.query("users").collect();
+  },
+});
